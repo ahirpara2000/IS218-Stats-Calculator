e5a71d6b23ab32a75fffcf46e057fc90ca58ffca
ahirpara2000 <ahirpara2000@gmail.com>
2021-03-07 15:58:36 -0500

    Feature: Added multiplication and division static method

--- Calculator/operations.py
+++ Calculator/operations.py
@@ -4,5 +4,14 @@ class Operations:
     def addition(a, b):
         return float(a) + float(b)
 
+    @staticmethod
     def subtraction(a, b):
         return float(b) - float(a)
+
+    @staticmethod
+    def multiplication(a, b):
+        return round(float(a) * float(b), 9)
+
+    @staticmethod
+    def division(a, b):
+        return round(float(b) / float(a), 9)
