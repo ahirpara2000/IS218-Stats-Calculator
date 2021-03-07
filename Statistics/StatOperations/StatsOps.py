098785e9def7d7696111a4dd3ac86f2f4cc57d8b
ahirpara2000 <ahirpara2000@gmail.com>
2021-03-07 17:38:48 -0500

    Task: Added mean function in Statistics, and created StasOps (Statistics operations) file

---
+++ Statistics/StatOperations/StatsOps.py
@@ -0,0 +1,10 @@
+from Calculator.Operations.operations import Operations
+
+class StatsOps:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def mean(data):
+        pass
--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,7 +1,12 @@
 from Calculator.Calculator import Calculator
-
+from Statistics.StatOperations.StatsOps import StatsOps
 
 class Statistics(Calculator):
+    data = []
 
     def __init__(self):
         super().__init__()
+
+    def mean(self, data):
+        self.result = StatsOps.mean(data)
+        return self.result
